6d14008cae96f7ee004182e9f06f40fa1ca093e7
Rekha Tenjarla <Rekha@Rekhas-MBP.fios-router.home>
2017-11-12 13:42:37 -0500

    first commit'

--- src/App.js
+++ src/App.js
@@ -30,13 +30,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2><Button bsStyle="primary">Welcome</Button> to React</h2>     
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
         <div ref='mapView' className='map-view'></div>
       </div>
     );
