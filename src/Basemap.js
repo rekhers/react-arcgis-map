07e5519c27e9db8ac2999197a06634015a6ac9ef
Rekha Tenjarla <Rekha@Rekhas-MBP.fios-router.home>
2017-11-13 21:08:40 -0500

    toggle works

--- src/Basemap.js
+++ src/Basemap.js
@@ -1,45 +1,21 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { createMap } from './actions/map';
-import FeatureLayer from 'esri/layers/FeatureLayer';
-import MapView from 'esri/views/MapView';
-import EsriMap from 'esri/Map';
 
     
-var basemap = new EsriMap({
-          basemap: "hybrid"
-        })
-
-
-const view = (domNode) => {
-    return new MapView({
-            container: domNode,
-            map:basemap,
-            extent: { // autocasts as new Extent()
-            xmin: -9177811,
-            ymin: 4247000,
-            xmax: -9176791,
-            ymax: 4247784,
-            spatialReference: 102100
-          }
-          });
-}
 
 
 
-const mapStateToProps = (state) => {
-  return {
-    mapCtrl: state.map.mapCtrl
-  }
-}
+// const mapStateToProps = (state) => {
+//   return {
+//     mapCtrl: state.map.mapCtrl
+//   }
+// }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    createMap: (view) => {
-      dispatch(createMap(view))
-    }
-  }
-}
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     createMap: (view) => {
+//       dispatch(createMap(view))
+//     }
+//   }
+// }
 
 
 // const addLayer = () =>{
@@ -52,43 +28,54 @@ const mapDispatchToProps = (dispatch) => {
 // }
 
 
-class Basemap extends Component{
-
-	constructor(props){
-		super(props);
-		this.state = this.props;
-	}
-
-	componentDidMount(){
-		if(!this.props.mapCtrl){
-			this.props.createMap(view(this.refs.mapView));
-		}
-
-	
-	}
-
-	shouldComponentUpdate(props){
-		console.log("props");
-		console.log(props);
-			var featureLayer = new FeatureLayer({
-          			url: "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0"
-      			  });
+// const removeLayer = () =>{
+// 	console.log("in add layer");
+// 		var featureLayer = new FeatureLayer({
+//           			url: "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0"
+//       			  });
  
-      		 basemap.add(featureLayer);
-		
-		return true;
-	}
+//       		 basemap.remove(featureLayer);
+// }
+
+// class Basemap extends Component{
+
+// 	constructor(props){
+// 		super(props);
+// 		this.state = this.props;
+// 	}
+
+// 	componentDidMount(){
+// 		if(!this.props.mapCtrl){
+// 			this.props.createMap(view(this.refs.mapView));
+// 		}
+			
+// 			addLayer();
+
+// 	}
+
+// 	shouldComponentUpdate(props){
+// 				console.log(this.props);
+
+// 				removeLayer();
+// 		// if(this.props.handleLayer === false){
+// 		// 	removeLayer();
+// 		// } else {
+// 		// 	addLayer();
+// 		// }
+
+// 		return true;
+// 	}
 
 
-	render(){
-		return(<div ref='mapView' className='map-view'></div>)
-	}
+// 	render(){
+// 		return(<div ref='mapView' className='map-view'></div>)
+// 	}
 
-}
+// }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Basemap);
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(Basemap);
 
 
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,6 +1,57 @@
 import React, { Component } from 'react';
-import Basemap from './Basemap';
+import { connect } from 'react-redux';
+import { createMap } from './actions/map';
+import FeatureLayer from 'esri/layers/FeatureLayer';
+import MapView from 'esri/views/MapView';
+import EsriMap from 'esri/Map';
 
+var featureLayer = new FeatureLayer({
+                url: "http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0"
+              });
+
+var basemap = new EsriMap({
+          basemap: "hybrid"
+        })
+
+
+const view = (domNode) => {
+    return new MapView({
+            container: domNode,
+            map:basemap,
+            extent: { // autocasts as new Extent()
+            xmin: -9177811,
+            ymin: 4247000,
+            xmax: -9176791,
+            ymax: 4247784,
+            spatialReference: 102100
+          }
+          });
+}
+
+const mapStateToProps = (state) => {
+  return {
+    mapCtrl: state.map.mapCtrl
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createMap: (view) => {
+      dispatch(createMap(view))
+    }
+  }
+}
+
+
+
+const addLayer = () =>{
+           basemap.add(featureLayer);
+}
+
+
+const removeLayer = () =>{
+           basemap.remove(featureLayer);
+}
 
 
 /*
@@ -12,36 +63,44 @@ class MapContainer extends Component{
 
   constructor(props){
     super(props);
-    this.state = {showLayer: this.props};
+    this.state = {handleLayer: false};
   }
 
 	componentDidMount(props){
-    this.toggleLayer(props);
+      if(!this.props.mapCtrl){
+      this.props.createMap(view(this.refs.mapView));
+    }
 	}
 
- 
+
+  shouldComponentUpdate(props){
+    console.log(props);
+
+    console.log(props.showLayer);
 
 
-  toggleLayer(){ 
-  if(this.props.showLayer === undefined){
-    this.props.showLayer = false;
-  }
 
-    if(this.props.showLayer === true){
-      console.log("true in toggle");
-      // addLayer();
-    } else {
-        console.log("false in toggle");
-
+    if(props.showLayer === true){
+      addLayer();
+    } else if(props.showLayer === false) {
+      console.log("are we getting here?!");
+      removeLayer();
     }
+    return true;
   }
+
+ 
+  
 
  
 	render(){
-		return (<Basemap showLayer={this.state.showLayer}/>)
+        return(<div ref='mapView' className='map-view'></div>)
+
 	}
 }
 
 
-export { MapContainer as default } 
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MapContainer);
